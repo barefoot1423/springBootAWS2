26557dad7848d60efb69c1999925f94d565133a9
Hyunwoo Cho <barefoot1423@gmail.com>
2020-04-26 19:53:56 -0400

    add findById method

--- src/main/java/com/hyunwoocho/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/hyunwoocho/book/springboot/service/posts/PostsService.java
@@ -2,6 +2,7 @@ package com.hyunwoocho.book.springboot.service.posts;
 
 import com.hyunwoocho.book.springboot.domain.posts.Posts;
 import com.hyunwoocho.book.springboot.domain.posts.PostsRepository;
+import com.hyunwoocho.book.springboot.web.dto.PostsResponseDto;
 import com.hyunwoocho.book.springboot.web.dto.PostsSaveRequestDto;
 import com.hyunwoocho.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
@@ -25,5 +26,10 @@ public class PostsService {
         posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
     }
 }
