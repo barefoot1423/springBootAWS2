797cd5ce7e5b6218b5d3d140d4b4bf91251c4305
Hyunwoo Cho <barefoot1423@gmail.com>
2020-04-26 19:49:29 -0400

    add update method

--- src/main/java/com/hyunwoocho/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/hyunwoocho/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,9 @@
 package com.hyunwoocho.book.springboot.service.posts;
 
+import com.hyunwoocho.book.springboot.domain.posts.Posts;
 import com.hyunwoocho.book.springboot.domain.posts.PostsRepository;
 import com.hyunwoocho.book.springboot.web.dto.PostsSaveRequestDto;
+import com.hyunwoocho.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +17,13 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
     }
 }
