533fb579bf39ef9832282f651c88d851369c4d7b
Hyunwoo Cho <barefoot1423@gmail.com>
2020-05-02 23:38:28 -0400

    add findAllDesc api with readOnly option

--- src/main/java/com/hyunwoocho/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/hyunwoocho/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.hyunwoocho.book.springboot.service.posts;
 
 import com.hyunwoocho.book.springboot.domain.posts.Posts;
 import com.hyunwoocho.book.springboot.domain.posts.PostsRepository;
+import com.hyunwoocho.book.springboot.web.dto.PostsListResponseDto;
 import com.hyunwoocho.book.springboot.web.dto.PostsResponseDto;
 import com.hyunwoocho.book.springboot.web.dto.PostsSaveRequestDto;
 import com.hyunwoocho.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,5 +35,12 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+            .map(PostsListResponseDto::new)
+            .collect(Collectors.toList());
     }
 }
